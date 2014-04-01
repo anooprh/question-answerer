e610fd64381d84cb6b54ac2ea35a41af1007f6d8
anooprh <anoop.hallur@gmail.com>
2014-03-31 20:37:32 -0400

    Work in progress

--- script.py
+++ script.py
@@ -9,5 +9,15 @@ questions = filter(lambda x: '?' in x, sentences)
 
 print questions
 
+my_questions = ['What is your favourite drink?', 'Where am I?', 'What is the temperature?', 'How old are you?']
+formatted_questions = [];
 
+for question in my_questions:
+    sentences = nltk.sent_tokenize(question)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences];
+    formatted_questions.append(sentences)
 
+for question in formatted_questions:
+
+pass
