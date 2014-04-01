85faec075a32fcc6e768b0fd785502f610fc5a69
anooprh <anoop.hallur@gmail.com>
2014-03-31 20:00:15 -0400

    some script working

--- script.py
+++ script.py
@@ -1,6 +1,13 @@
 import nltk
+import re
 
 fo = open('story.txt', 'r')
 text = fo.read()
+text = re.sub('\n', ' ', text)
 sentences = nltk.sent_tokenize(text)
+questions = filter(lambda x: '?' in x, sentences)
+
+print questions
+
+
 
